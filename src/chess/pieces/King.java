daca5aa18a4b5b6c9c1454ecafa64a416be4ef02
Muzashi <evertoncosta100@gmail.com>
2021-05-11 20:15:05 -0300

    BoardException and defensive programming

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -9,8 +9,6 @@ public class King extends ChessPiece{
 	public King(Board board, Color color) {
 		super(board, color);
 	}
-	
-	@Override
 	public String toString() {
 		return "K";
 	}
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -10,7 +10,6 @@ public class Rook extends ChessPiece{
 		super(board, color);
 	}
 	
-	@Override
 	public String toString() {
 		return "R";
 	}
